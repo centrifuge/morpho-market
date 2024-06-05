b5fe04010835bc1eafa7ea37b8660393a83c90bc
Adam Stox <adamstox@protonmail.com>
2024-06-05 12:05:34 +0100

    Add memberlist and tests

---
+++ src/Memberlist.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.21;
+
+import {Auth} from "./utils/Auth.sol";
+
+contract Memberlist is Auth {
+    // --- Storage ---
+
+    mapping(address => uint) public members;
+
+    // --- Events ---
+
+    event MemberAdded(address member);
+    event MemberRemoved(address member);
+
+    // --- Constructor ---
+
+    constructor() {
+        wards[msg.sender] = 1;
+        emit Rely(msg.sender);
+    }
+
+    // --- External ---
+
+    function addMember(address member) external auth {
+        require(members[member] == 0, "Memberlist: member already exists");
+        members[member] = 1;
+        emit MemberAdded(member);
+    }
+
+    function removeMember(address member) external auth {
+        require(members[member] == 1, "Memberlist: member does not exist");
+        members[member] = 0;
+        emit MemberRemoved(member);
+    }
+
+    function isMember(address member) external view returns (bool) {
+        return members[member] == 1;
+    }
+}
