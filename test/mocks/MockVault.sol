e3df3ec09ffae34684612ba6616500d4089ceda7
Adam Stox <adamstox@protonmail.com>
2024-06-07 17:05:42 +0100

    fix tests

--- test/mocks/MockVault.sol
+++ test/mocks/MockVault.sol
@@ -4,16 +4,21 @@ pragma solidity 0.8.21;
 import "test/mocks/BaseMock.sol";
 
 contract MockVault is BaseMock {
-    constructor(uint128 price) {
+    constructor(uint128 price, uint8 shareDecimals, address asset) {
         values_uint128["price"] = price;
-        values_uint256["shareDecimals"] = 18;
+        values_uint8["shareDecimals"] = shareDecimals;
+        values_address["asset"] = asset;
     }
 
     function convertToAssets(uint256 amount) external returns (uint128) {
         return values_uint128["price"];
     }
 
-    function shareDecimals() external view returns (uint256) {
-        return values_uint256["shareDecimals"];
+    function shareDecimals() external view returns (uint8) {
+        return values_uint8["shareDecimals"];
+    }
+
+    function asset() external view returns (address) {
+        return values_address["asset"];
     }
 }
--- test/unit/Oracle.t.sol
+++ test/unit/Oracle.t.sol
@@ -3,17 +3,24 @@ pragma solidity ^0.8.13;
 import {Test} from "forge-std/Test.sol";
 import {Oracle} from "src/Oracle.sol";
 import {MockVault} from "test/mocks/MockVault.sol";
+import {ERC20} from "src/ERC20.sol";
+import {MathLib} from "src/libraries/MathLib.sol";
+import {IERC20Metadata} from "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract OracleTest is Test {
     Oracle oracle;
 
     function setUp() public {}
 
-    function test_Price(uint128 price) public {
-        MockVault vault = new MockVault(price);
+    function test_Price(uint128 price, uint8 shareDecimals) public {
+        vm.assume(shareDecimals <= 54);
+        ERC20 asset = new ERC20("Test", "TST");
+        MockVault vault = new MockVault(price, shareDecimals, address(asset));
         oracle = new Oracle(address(vault));
         uint256 oraclePrice = oracle.price();
-        assertEq(oraclePrice, price);
+        uint8 assetDecimals = IERC20Metadata(vault.asset()).decimals();
+        uint8 precisionDifference = 36 + assetDecimals - vault.shareDecimals();
+        uint256 expectedPrice = MathLib.mulDiv(price, 10 ** precisionDifference, 10 ** assetDecimals);
+        assertEq(oraclePrice, expectedPrice);
     }
 }
-
