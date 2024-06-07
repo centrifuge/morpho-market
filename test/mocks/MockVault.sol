ed5b03c9795474fd93840892d263815d6bc6bd93
Adam Stox <adamstox@protonmail.com>
2024-06-07 14:17:13 +0100

    add simple mockVault

---
+++ test/mocks/MockVault.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.21;
+
+import "./Mock.sol";
+
+contract MockVault is Mock {
+    constuctor (uint128 price) {
+        values_uint128["price"] = price;
+    }
+
+    function convertToAssets(uint256 amount) external {
+        return values_uint128["price"];
+    }
+}
