4813d805f8337fb9dd501a4109fdbdcc31fc06d1
ilin <alina@k-f.co>
2024-06-11 22:20:20 +0100

    add more tests to Oracle

--- src/VaultOracle.sol
+++ src/VaultOracle.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {IOracle} from "src/interfaces/IOracle.sol";
 import {IERC20Metadata} from "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import {MathLib} from "src/libraries/MathLib.sol";
+import {Auth} from "liquidity-pools/src/Auth.sol";
 
 interface IERC7540Vault {
     function share() external view returns (address share);
@@ -11,16 +11,28 @@ interface IERC7540Vault {
     function convertToAssets(uint256 shares) external view returns (uint256 assets);
 }
 
-contract VaultOracle is IOracle {
+contract VaultOracle is IOracle, Auth {
     IERC7540Vault public vault;
-    uint8 constant PRICE_DECIMALS = 36;
-    uint8 assetDecimals;
-    uint8 shareDecimals;
+    uint8 public constant PRICE_DECIMALS = 36;
+    uint8 public assetDecimals;
+    uint8 public shareDecimals;
+
+    // --- Events ---
+    event File(bytes32 indexed what, address data);
 
     constructor(address vault_) {
-        vault = IERC7540Vault(vault_);
-        assetDecimals = IERC20Metadata(vault.asset()).decimals();
-        shareDecimals =  IERC20Metadata(vault.share()).decimals();
+        _updateVault(vault_);
+        wards[msg.sender] = 1;
+    }
+
+    // --- Administration ---
+    function file(bytes32 what, address data) external auth {
+        if (what == "vault") {
+            _updateVault(data);
+            emit File(what, data);
+        } else {
+            revert("VaultOracle/file-unrecognized-param");
+        }
     }
 
     function price() external view override returns (uint256 price) {
@@ -28,5 +40,11 @@ contract VaultOracle is IOracle {
         if (assetDecimals == PRICE_DECIMALS) price = priceInAssetDecimals;
         else if (assetDecimals > PRICE_DECIMALS) price = priceInAssetDecimals / 10 ** (assetDecimals - PRICE_DECIMALS);
         else price = priceInAssetDecimals * 10 ** (PRICE_DECIMALS - assetDecimals);
+    }
+
+    function _updateVault(address vault_) internal {
+        vault = IERC7540Vault(vault_);
+        assetDecimals = IERC20Metadata(vault.asset()).decimals();
+        shareDecimals = IERC20Metadata(vault.share()).decimals();
     }
 }
--- test/unit/Oracle.t.sol
+++ test/unit/Oracle.t.sol
@@ -5,23 +5,63 @@ import "forge-std/console.sol";
 import {VaultOracle} from "src/VaultOracle.sol";
 import {MockVault} from "test/mocks/MockVault.sol";
 import {ERC20} from "src/ERC20.sol";
-import {MathLib} from "src/libraries/MathLib.sol";
 import {IERC20Metadata} from "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {Auth} from "liquidity-pools/src/Auth.sol";
 
 contract OracleTest is Test {
     VaultOracle oracle;
 
     function setUp() public {}
 
+    function test_Deploy(uint8 assetDecimals, uint8 shareDecimals) public {
+        vm.assume(assetDecimals <= 54 && assetDecimals > 0);
+        vm.assume(shareDecimals <= 54 && shareDecimals > 0);
+        deployOracle(1, assetDecimals, shareDecimals);
+    }
+
+    function test_File(uint8 assetDecimals, uint8 shareDecimals, uint8 assetDecimalsNew, uint8 shareDecimalsNew)
+        public
+    {
+        vm.assume(assetDecimals <= 54 && assetDecimals > 0);
+        vm.assume(shareDecimals <= 54 && shareDecimals > 0);
+        vm.assume(assetDecimalsNew <= 54 && assetDecimalsNew > 0);
+        vm.assume(shareDecimalsNew <= 54 && shareDecimalsNew > 0);
+        deployOracle(1, assetDecimals, shareDecimals);
+
+        // update vault
+        MockVault vaultNew = new MockVault(1, shareDecimalsNew, assetDecimalsNew);
+        oracle.file("vault", address(vaultNew));
+        assertEq(Auth(oracle).wards(address(this)), 1);
+        assert(address(oracle.vault()) == address(vaultNew));
+        assertEq(oracle.assetDecimals(), assetDecimalsNew);
+        assertEq(oracle.shareDecimals(), shareDecimalsNew);
+        // fail to file unknown param
+        vm.expectRevert("VaultOracle/file-unrecognized-param");
+        oracle.file("random", address(vaultNew));
+        // fail to file not ward
+        oracle.deny(address(this));
+        vm.expectRevert("Auth/not-authorized");
+        oracle.file("vault", address(vaultNew));
+    }
+
     function test_Price(uint256 price, uint8 assetDecimals, uint8 shareDecimals) public {
         vm.assume(assetDecimals <= 54 && assetDecimals > 0);
         vm.assume(shareDecimals <= 54 && shareDecimals > 0);
         vm.assume(price < 1000);
-
-        MockVault vault = new MockVault(price, shareDecimals, assetDecimals);
-        oracle = new VaultOracle(address(vault));
+        deployOracle(price, assetDecimals, shareDecimals);
         uint256 oraclePrice = oracle.price();
 
         assertEq(oraclePrice, price * 10 ** 36);
+    }
+
+    function deployOracle(uint256 price, uint8 assetDecimals, uint8 shareDecimals) public {
+        MockVault vault = new MockVault(price, shareDecimals, assetDecimals);
+        oracle = new VaultOracle(address(vault));
+        // assert permissions
+        assertEq(Auth(oracle).wards(address(this)), 1);
+        // assert state
+        assert(address(oracle.vault()) == address(vault));
+        assertEq(oracle.assetDecimals(), assetDecimals);
+        assertEq(oracle.shareDecimals(), shareDecimals);
     }
 }
