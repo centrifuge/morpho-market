1d14854ab87d3a068be207a342b2b5d69ac5bfd6
Adam Stox <adamstox@protonmail.com>
2024-06-20 18:27:15 +0100

    Update deploy script and .example.env

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,6 +5,7 @@ import {Script} from "forge-std/Script.sol";
 import {Memberlist} from "src/Memberlist.sol";
 import {PermissionedERC20Wrapper} from "src/PermissionedERC20Wrapper.sol";
 import {ERC20PermissionedBase, IERC20} from "lib/erc20-permissioned/src/ERC20PermissionedBase.sol";
+import {VaultOracle} from "src/VaultOracle.sol";
 
 contract DeployScript is Script {
     address USDC = vm.envAddress("UNDERLYING_TOKEN");
@@ -13,15 +14,17 @@ contract DeployScript is Script {
     address attestationService = vm.envAddress("ATTESTATION_SERVICE");
     address attestationIndexer = vm.envAddress("ATTESTATION_INDEXER");
     address admin = vm.envAddress("ADMIN");
+    address vault = vm.envAddress("VAULT");
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
+        VaultOracle oracle;
         Memberlist memberlist = new Memberlist();
         PermissionedERC20Wrapper wrappedUSDC = new PermissionedERC20Wrapper(
-            "Attested USDC",
-            "aUSDC",
+            "Verified USDC",
+            "verUSDC",
             IERC20(USDC),
             morpho,
             bundler,
@@ -29,11 +32,18 @@ contract DeployScript is Script {
             attestationIndexer,
             address(memberlist)
         );
+        if (vault != address(0)) {
+            oracle = new VaultOracle(vault);
+        }
         if (admin != msg.sender && admin != address(0)) {
             memberlist.rely(admin);
             wrappedUSDC.rely(admin);
             memberlist.deny(msg.sender);
             wrappedUSDC.deny(msg.sender);
+            if (address(oracle) != address(0)) {
+                oracle.rely(admin);
+                oracle.deny(msg.sender);
+            }
         }
         vm.stopBroadcast();
     }
